dc96cf7e0c74a5a8566413a0ac538b52ff96be05
Alejandro Sendín <asendinv@gmail.com>
2026-07-25 12:28:03 +0200

    Made scripts to retrieve TCGA and CBioPortal cohort information into R.

---
+++ Analysis_TCGA-STAD_data.R
@@ -0,0 +1,37 @@
+# Install packages CBIO --------------------------------------------------------
+bioc_packages <- c(
+  "cBioPortalData",
+  "TCGAbiolinks",
+  "SummarizedExperiment",
+  "maftools"
+)
+
+for (pkg in bioc_packages) {
+  if (!requireNamespace(pkg, quietly = TRUE))
+    BiocManager::install(pkg)
+}
+
+# libraries --------------------------------------------------------------------
+library(cBioPortalData)
+library(TCGAbiolinks)
+library(SummarizedExperiment)
+library(maftools)
+
+# directory definition ---------------------------------------------------------
+cbio_dir <- Sys.getenv("CBIO_DATA")
+
+# Retreiving cBioPortal data ---------------------------------------------------
+# (CBIOPORTAL QUERY SCRIPT)
+
+# directory definition ---------------------------------------------------------
+tcga_dir <- Sys.getenv("TCGA_DATA")
+
+# Retreiving TCGA data from .rds files -----------------------------------------
+clinical <- readRDS(file.path(tcga_dir, "Prepared", "TCGA_STAD_clinical.rds"))
+cnv_se <- readRDS(file.path(tcga_dir, "Prepared", "TCGA_STAD_cnv_se.rds"))
+maf <- readRDS(file.path(tcga_dir, "Prepared", "TCGA_STAD_maf.rds"))
+methylation_450K_se <- readRDS(
+  file.path(tcga_dir, "Prepared", "TCGA_STAD_methylation_450K_se.rds")
+)
+mirna_se <- readRDS(file.path(tcga_dir, "Prepared", "TCGA_STAD_mirna_se.rds"))
+rnaseq_se <- readRDS(file.path(tcga_dir, "Prepared", "TCGA_STAD_rnaseq_se.rds"))
--- CBIOPORTAL_query_TCGA-STAD_clinical.R
+++ CBIOPORTAL_query_TCGA-STAD_clinical.R
@@ -35,3 +35,66 @@ for (id in study_ids) {
     ask = FALSE
   )
 }
+
+# Retreiving cBioPortal data ---------------------------------------------------
+# idealmente se haría un multiassay:
+
+#stad_tcga_pub <- cBioDataPack(
+#  cancer_study_id = "stad_tcga_pub",
+#  use_cache = cbio_dir,
+#  cleanup = TRUE,
+#  ask = FALSE
+#)
+
+# pero no funciona, da error con el fichero de stad_tcga_pub.
+# Queda la opción de ir obteniendo los archivos uno a uno. Al descomprimir el
+# archivo se obtienen diferentes datos. Para saber qué datos es de interés importar
+# se obtienen los nombres de todas las columnas:
+
+# se redefine stad_dir ya que se ha generado una carpeta extra al descomprimir:
+stad_dir <- file.path(cbio_dir, "stad_tcga_pub", "stad_tcga_pub")
+
+# CHECK COLUMNAS ---------------------------------------------------------------
+# all file rutes are retrieved from the decompresssed original file 
+files <- list.files(
+  stad_dir,
+  pattern = "\\.(txt|seg)$",
+  full.names = TRUE
+)
+
+# se obtiene un objeto con los archivos y sus columnas
+columns <- lapply(files, function(file) {
+  names(read.delim(
+    file,
+    comment.char = "#",
+    nrows = 0,
+    check.names = FALSE
+  ))
+})
+
+names(columns) <- basename(files)
+columns
+
+# en principio se puede ver que se va a necesitar: data_clinical_patient, 
+# data_clinical_sample, data_cna (copy number alterations), data_linear_cna, 
+# data_mrna_seq_v2_rsem, data_mutations.
+
+# OBTENCIÓN DATOS --------------------------------------------------------------
+clinical_patient <- read.delim(file.path(stad_dir, "data_clinical_patient.txt"),
+                               comment.char = "#",check.names = FALSE)
+
+clinical_sample <- read.delim(file.path(stad_dir, "data_clinical_sample.txt"),
+                              comment.char = "#",check.names = FALSE)
+
+cna <- read.delim(file.path(stad_dir, "data_cna.txt"),check.names = FALSE)
+
+cna_linear <- read.delim(file.path(stad_dir, "data_linear_cna.txt"),
+                         check.names = FALSE)
+
+mrna <- read.delim(file.path(stad_dir, "data_mrna_seq_v2_rsem.txt"),
+                   check.names = FALSE)
+
+mut <- read.delim(file.path(stad_dir, "data_mutations.txt"),comment.char = "#",
+                  check.names = FALSE)
+
+
