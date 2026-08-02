24d897f2c141adf93421afc307104e54cc73cda4
Alejandro Sendín <asendinv@gmail.com>
2026-08-02 13:29:56 +0200

    Applied 2 dif clustering methods, now doing dif expression of clusters.

--- Analysis_TCGA-STAD_data.R
+++ Analysis_TCGA-STAD_data.R
@@ -26,6 +26,11 @@ library(dplyr)
 library(pheatmap)
 library(data.table)
 library(dataframeexplorer)
+library(devtools)
+library(ComplexHeatmap)
+library(cluster)
+library(edgeR)
+library(limma)
 
 # directory definition ---------------------------------------------------------
 cbio_dir <- Sys.getenv("CBIO_DATA")
@@ -109,6 +114,7 @@ assayNames(rnaseq_se)
 tpm <- assay(rnaseq_se, "tpm_unstrand") # todas las muestras del proyecto TCGA-STAD con datos rna-seq
 dim(tpm)
 # esto es 448 muestras
+summary(colSums(tpm)) # paso extra cortesía de GPT, debería sumar 10^6
 sum(tpm == 0, na.rm = TRUE)
 # hay bastantes 0
 # eliminamos las muestras con valor 0 en todos sus genes
@@ -124,27 +130,21 @@ tpm_tumor <- tpm_filt[, substr(colnames(tpm_filt), 14, 15) == "01", drop = FALSE
 dim(tpm_tumor)
 # hay 412 muestras de tumor
 
-# Ahora ya se puede hacer un filtraje de los genes más representativos
-# eliminamos los genes con valor 0 en todas sus muestras
-tpm_tumor <- tpm_tumor[, colSums(tpm_tumor > 0) > 0, drop = FALSE]
-dim(tpm_tumor)
-# no hay genes con valor 0 en todas las muestras
 # ahora se mantendran los genes con un tpm >= 1 en el 25% de las muestras
 # se calcula qué número es el 25%
-min <- ceiling(0.25*ncol(tpm_tumor))
-# ahora buscamos el dataset en el que los valores sean >= 1 en 42 columnas
-genes_exp <- rowSums(tpm_tumor >= 1) >= min
-# nos queda un vector de valores TRUE/FALSE por fila según si cumple o no las condiciones
+n_min <- ceiling(0.25*ncol(tpm_tumor))
+# ahora buscamos el dataset en el que los valores sean >= 1 en 103 columnas o más
+genes_exp <- rowSums(tpm_tumor >= 1) >= n_min
+# nos queda un objeto con valores TRUE/FALSE con su fila según si cumple o no las condiciones
 tpm_filt <- tpm_tumor[genes_exp, , drop = FALSE]
 dim(tpm_filt)
 # quedan 21k genes
 # aquí se deberían evaluar si hay outliers o batch effect
 # se transforman los datos a log
 tpm_filt_log <- log2(tpm_filt + 1)
-# Ahora se filtra por variabilidad de expresión siendo el cv = std dev/media y 
-# buscando el 25% más variable
-gene_mad <- apply(tpm_filt_log, 1, mad) # esto da un vector en el hay las Median
-# Absolute Deviation de cada fila (gen) (1) de la matrix indicada
+# Ahora se filtra por variabilidad de expresión (median absolute deviation) buscando el 25% más variable
+gene_mad <- apply(tpm_filt_log, 1, mad) # esto da un vector de mad aplicado a cada fila (1) de la matrix indicada
+# igual que genes_exp pero numérico en vez de lógico
 top <- ceiling(0.25*nrow(tpm_filt_log)) # se obtiene el nº de genes = 25%
 ind <- order(gene_mad, decreasing = TRUE)[seq_len(top)] 
 # order devuelve la posición
@@ -156,36 +156,68 @@ tpm_filt <- tpm_filt_log[ind, , drop=FALSE]
 dim(tpm_filt)
 # quedan 5349 genes
 
-library(devtools)
-install_github("jokergoo/ComplexHeatmap")
+# 1- separar estas muestras en clústers
+
+# las filas son los elementos finales del clúster, como queremos clusterizar muestras
+# (columnas) habría que transponer la matriz para que la disimilitud se calcule entre los genes
+
+# primero se transpone la matriz, se hace el z-score y se vuelve a transponer de nuevo.
+tpm_z <- t(scale(t(tpm_filt_log)))
+sum(is.na(tpm_z))
+
+# clustering más habitual: euclidean + ward.D2. Distancia + clust
+d <- dist(t(tpm_z), method = "euclidean")
+hcl <- hclust(d, method = "ward.D2")
+plot(hcl, labels = FALSE, hang = -1, main = "Hierarchical clust: euc + wardd2", xlab = "muestras")
+
+# clustering 2: 1-Pearson + average, mide correlaciones entre rows y hace el average?
+# se calcula la correlacion y la dist
+corPearson <- cor(tpm_z, method = "pearson", use = "pairwise.complete.obs")
+dist_pearson <- as.dist(1 - corPearson)
+# se hace el cluster
+hcl_p <- hclust(dist_pearson, method = "average")
+plot(hcl_p, labels = FALSE, hang = -1, main = "Hierarchical clust: 1-Pearson + average", xlab = "muestras")
+
+# aparentemente k = 4 parece lo mejor
+# veamos la n en cada clúster para cada método
+clusters <- cutree(hcl, k = 4)
+table(clusters)
+
+clustersp <- cutree(hcl_p, k = 4)
+table(clustersp)
+
+# aquí se ve que el clúster 1 varía un poco entre los diferentes métodos. 
+# me surge una duda, cómo se puede estudiar esta diferencia??? -----------------
+# por ahora se usará eucliden+ward.D2
+
+# 2- buscar los genes diferencialmente más expresados de cada clúster (~10)
+# ahora hay que hacer el análisis de expresión diferencial por cluster
+# obtenemos los datos de conteos crudos para poder usar mejor edger y limma
+counts <- assay(rnaseq_se, "unstranded")
+# se obtienen solo las muestras de clusters
+counts <- counts[, names(clusters), drop = FALSE]
+stopifnot(identical(colnames(counts), names(clusters))) # para ver facilmente que los nombres sean iguales
+# se crea el objeto DGEList
+# primero se obtiene el grupo que hace de "meta" de las muestras
+cluster <- factor(clusters[colnames(counts)], levels = 1:4, labels = c("C1","C2","C3","C4"))
+# se crea el DGEList
+dge <- DGEList(counts = counts, group = cluster)
+# se hace un filtrado de genes con la función siguiente:
+keep <- filterByExpr(dge, group = cluster)
+# si se elimina un gen, recalcula los counts por muestra
+dge <- dge[keep, , keep.lib.sizes = FALSE] 
+dge <- calcNormFactors(dge, method = "TMM")
+# se hace la matriz de diseño siguiendo el modelo cluster
+design <- model.matrix(~ 0 + cluster)
+colnames(design) <- levels(cluster)
+# se crea el voom
+v <- voom(dge, design)
+fit <- lmFit(v, design)
 
 
 
-pheatmap(
-  tpm_filt,
-  scale = "row",
-  cluster_rows = TRUE,
-  cluster_cols = TRUE,
-  clustering_distance_rows = "euclidean",
-  clustering_distance_cols = "euclidean",
-  clustering_method = "ward.D2",
-  show_rownames = FALSE,
-  show_colnames = FALSE,
-  main = "TCGA-STAD: 25% de genes con mayor MAD"
-)
+# 3- hacer el heatmap con esos genes 
 
+# CUESTIONES: separar por clústers y buscar los que tienen una expresión diferencial mayor en ellos
 
-media_fpkm <- rowMeans(fpkm_filt, na.rm = TRUE)
-keep_mean <- is.finite(media_fpkm) & media_fpkm >= 10
-fpkm_mean10 <- fpkm_filt[keep_mean, , drop = FALSE]
-media_mean10 <- media_fpkm[keep_mean]
-sd_fpkm <- apply(fpkm_mean10, 1, sd, na.rm = TRUE)
-cv_fpkm <- sd_fpkm / media_mean10
-keep_cv <- is.finite(cv_fpkm)
-fpkm_cv <- fpkm_mean10[keep_cv, , drop = FALSE]
-cv_fpkm <- cv_fpkm[keep_cv]
-orden_cv <- order(cv_fpkm, decreasing = TRUE)
-n_top <- ceiling(0.25 * nrow(fpkm_cv))
-top_idx <- orden_cv[seq_len(n_top)]
-fpkm_top25 <- fpkm_cv[top_idx, , drop = FALSE]
 
