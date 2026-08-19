a847388510de062785f80deeb97f1aaed543d7af
Alejandro Sendín <asendinv@gmail.com>
2026-08-19 23:22:53 +0200

    Updated new clustering

--- Analysis_TCGA-STAD_data.R
+++ Analysis_TCGA-STAD_data.R
@@ -210,6 +210,8 @@ cont_PC2 <- get_top_genes(pca_outliers, "PC2", gene_map, n = 10)
 cont_PC1
 cont_PC2
 
+# pendiente ver si estos genes se asocian con alguna característica concreta
+
 # ------------------------------------------------------------------------------
 # DESCUBRIMIENTO DE LOS CLÚSTERS # ---------------------------------------------
 # ------------------------------------------------------------------------------
@@ -227,6 +229,18 @@ tpm_ind <- tpm_filt_log[ind, , drop=FALSE]
 dim(tpm_ind)
 # quedan 5349 genes
 
+# tras probar diferentes clusterizaciones ambiguas, creo que la clave puede estar
+# en reducir más el número de genes, ya que 5349 genes es un número alto comparado
+# con los artículos que estoy encontrando.
+ind_nmf <- order(gene_mad, decreasing = TRUE)[seq_len(1500)] 
+# se obtienen las posiciones (filas) del 1 al 1500
+# se limita el dataset a las filas que hay en ind
+tpm_ind <- tpm_filt_log[ind_nmf, , drop=FALSE]
+dim(tpm_ind)
+# quedan 1500 genes
+# porqué 1500? porque si, es arbitrario y aprox lo que escogen en el estudio de 
+# referencia
+
 # separar estas muestras en clústers
 # como queremos clusterizar muestras (columnas) habría que transponer la matriz 
 # para que la disimilitud se calcule usando los genes como variables
@@ -234,6 +248,7 @@ dim(tpm_ind)
 # primero se transpone la matriz, se hace el z-score y se vuelve a transponer de nuevo.
 tpm_z <- t(scale(t(tpm_ind)))
 sum(is.na(tpm_z))
+dim(tpm_z)
 
 # clustering 1: euclidean + ward.D2. Distancia + clust
 d <- dist(t(tpm_z), method = "euclidean")
@@ -249,11 +264,11 @@ hcl_p <- hclust(dist_pearson, method = "average")
 plot(hcl_p, labels = FALSE, hang = -1, main = "Hierarchical clust: 1-Pearson + average", xlab = "muestras")
 
 # clustering 3: ConsensusClusterPlus + euclidean/ward.d2
-ccp_input <- as.matrix(tpm_z)
+cc_input <- as.matrix(tpm_z)
 cc_dir <- file.path(getwd(), "output", "consensus_euclidean_wardD2")
 cc_dir <- normalizePath(cc_dir, winslash = "/", mustWork = TRUE)
 cc_results <- ConsensusClusterPlus(
-  d             = ccp_input,
+  d             = cc_input,
   maxK          = 6,             # evalúa k = 2,...,6
   reps          = 500,           # 500 remuestreos
   pItem         = 0.80,          # 80% de las muestras en cada repetición
@@ -272,7 +287,7 @@ cc_results <- ConsensusClusterPlus(
 cc_dir2 <- file.path(getwd(), "output", "consensus_pearson_average")
 cc_dir2 <- normalizePath(cc_dir2, winslash = "/", mustWork = TRUE)
 cc_resultsp <- ConsensusClusterPlus(
-  d             = ccp_input,
+  d             = cc_input,
   maxK          = 6,             # evalúa k = 2,...,6
   reps          = 500,           # 500 remuestreos
   pItem         = 0.80,          # 80% de las muestras en cada repetición
@@ -287,39 +302,25 @@ cc_resultsp <- ConsensusClusterPlus(
   writeTable    = FALSE,
   verbose       = TRUE
 )
+
 # ------------------------------------------------------------------------------
 # ------------------------------------------------------------------------------
+
 # Al final tras probar diferentes métodos de clusterización con tpm_z:
-# 1- cada método obtiene unas agrupaciones concretas
-# 2- no hay una solidez en cuanto a las agrupaciones, algunos clústers son difusos
-# 3- el consensus con pearson obtiene un mejor perfil con k = 3
+# no hay una solidez en cuanto a las agrupaciones, algunos clústers son difusos
+# el consensus con pearson obtiene una clasificación algo más clara, pero el número 
+# de k a escoger no parece demasiado claro, presenta codo difuso en k = 6.
 
-# por ende, pienso que el problema de esta baja concordancia viene dada por el número
-# de genes, el cual creo que puede ser excesivamente alto y dificulte la agrupación
-# en clústers.
-
-# cambio de planes, se prueba el método del artículo pero con las 412 muestras
-# primero creamos la matrix NMF (Non-negative matrix factorization), se vuelve 
-# al punto donde se creaba gene_mad y se modifica:
-
-ind_nmf <- order(gene_mad, decreasing = TRUE)[seq_len(1500)] 
-# se obtienen las posiciones (filas) del 1 al 1500
-# se limita el dataset a las filas que hay en ind
-tpm_nmf <- tpm_filt_log[ind_nmf, , drop=FALSE]
-dim(tpm_nmf)
-# quedan 1500 genes
-# porqué 1500? porque si, es arbitrario y aprox lo que escogen en el estudio de 
-# referencia
-# evaluación del número de clústeres x algoritmo de brunet
+# cambio de planes, se prueba el método del artículo, evaluación del número de 
+# clústeres x algoritmo de brunet con el paquete NMF
 nmf_rank <- nmfEstimateRank(
-  x      = tpm_nmf,
+  x      = tpm_ind,
   range  = 3:6,
   method = "brunet",
   nrun   = 30,
   seed   = 1234)
 
 plot(nmf_rank)
-nmf_rank$measures
 
 # A partir de los resultados obtenidos lo más importante es:
 # cophenetic = reproductibilidad asignaciones
@@ -341,14 +342,14 @@ criteria_k
 
 # viendo las imágenes de los diferentes consensusmaps y gráficos y por las métricas obtenidas
 # se podría escoger un valor de k = 3 o 6, ya que los valores diana disminuyen en k = 4 y 5.
-# tampoco es que sean valores malos... es difícil, tras hacer diferentes test planteados por GPT,
-# me queda la opción de hacer una consulta a claude para comparar o bien elegir
-# una clusterización según mi criterio científico ya que el criterio numérico/objetivo
-# es muy difuso y no es comparable a los otros métodos de clusterización realizados.
+# tampoco es que sean valores malos... es difícil, pasa lo mismo que con consensusclusterplus
+# tras hacer diferentes test planteados por GPT, me queda la opción de hacer una
+# consulta a claude para comparar o bien elegir una clusterización según mi criterio
+# ya que el criterio numérico/objetivo no es del todo concluyente.
 
-# creo que me decanto por hacer k = 6 porque a malas permite hacer una disección
-# más detallada, con mayor resolución. Al final dado que el código es más o menos
-# idéntico en cualquier escenario, siempre se puede cambiar y revisar los resultados.
+# creo que me decanto por hacer k = 6 porque a malas permite obtener mayor resolución. 
+# Al final dado que el código es más o menos idéntico en cualquier escenario, 
+# siempre se puede cambiar y revisar los resultados.
 
 # ------------------------------------------------------------------------------
 # CARACTERIZACIÓN DE LOS CLÚSTERS # --------------------------------------------
@@ -368,24 +369,25 @@ criteria_k
 # se eliminarían algunas muestras, quizá un 30%, que ya está bien.
 
 # se obtienen los datos de k = 6
-fit_k <- nmf_rank$fit[["6"]]
+fit <- nmf_rank$fit[["6"]]
 # asignación final basada en la matriz de consenso, se obtienen los clusters
-clusters_k <- predict(fit_k, what = "consensus")
+clusters <- predict(fit, what = "consensus")
 
-sil_k <- silhouette(fit_k, what = "consensus")
+names(clusters)  # barcodes
+clusters         # números de clúster
+
+sil <- silhouette(fit, what = "consensus")
 # se convierte sil_k en data.frame
 sil_table <- data.frame(
-  sample = rownames(sil_k),
-  cluster = factor(sil_k[, "cluster"]),
-  neighboring_cluster = factor(sil_k[, "neighbor"]),
-  silhouette_width = as.numeric(sil_k[, "sil_width"]),
+  sample = rownames(sil),
+  cluster = factor(sil[, "cluster"]),
+  neighboring_cluster = factor(sil[, "neighbor"]),
+  silhouette_width = as.numeric(sil[, "sil_width"]),
   row.names = NULL
 )
 
 # hora de escoger las muestras core. Criterio?
-
-
-
+# pendiente preguntar porque no lo acabo de ver claro.
 
 # buscar los genes diferencialmente más expresados de cada clúster (~10)
 # ahora hay que hacer el análisis de expresión diferencial por cluster
@@ -396,12 +398,11 @@ dim(counts)
 counts <- counts[, names(clusters), drop = FALSE]
 dim(counts)
 stopifnot(identical(colnames(counts), names(clusters))) # para ver fácilmente que los nombres sean iguales
-
 # se crea el objeto DGEList: ojo, en vez de hacer genes x muestras, se realiza el análisis
 # de expresión genes x clústers
 # primero se obtiene el vector categórico que hace de "meta" de las muestras
 # haces factor() de clusters cogiendo la estructura de counts, indicando los niveles y etiquetas
-cluster <- factor(clusters[colnames(counts)], levels = 1:4, labels = c("C1","C2","C3","C4"))
+cluster <- factor(clusters[colnames(counts)], levels = 1:6, labels = paste0("C", 1:6))
 dge <- DGEList(counts = counts, group = cluster)
 # ---------
 # se hace un filtrado de genes preventivo con filterByExpr(). Filtrado por defecto, sin más.
@@ -425,14 +426,16 @@ fit <- lmFit(v, design)
 # se organizan los contrastes entre clusters, se compara la expresión de cada clúster
 # con la media de expresiones del resto... es una forma pero hay otras
 # sintaxis es: diferentes contrastes como un vector + los niveles = matriz de diseño.
-contrasts <- makeContrasts(C1all = C1 - (C2+C3+C4)/3,
-                           C2all = C2 - (C1+C3+C4)/3,
-                           C3all = C3 - (C1+C2+C4)/3,
-                           C4all = C4 - (C1+C2+C3)/3, levels = design)
+contrasts_mat <- makeContrasts(C1all = C1 - (C2+C3+C4+C5+C6)/5,
+                           C2all = C2 - (C1+C3+C4+C5+C6)/5,
+                           C3all = C3 - (C1+C2+C4+C5+C6)/5,
+                           C4all = C4 - (C1+C2+C3+C5+C6)/5,
+                           C5all = C5 - (C1+C2+C3+C4+C6)/5,
+                           C6all = C6 - (C1+C2+C3+C4+C5)/5, levels = design)
 # contrasts.fit() coge un ajuste de un modelo lineal (fit) y una matriz con filas
 # que equivalen a las columnas de los coeficientes de fit y columnas que equivalen a los contrastes?
 # realiza el ajuste con contrastes
-fit_clusters <- contrasts.fit(fit, contrasts)
+fit_clusters <- contrasts.fit(fit, contrasts_mat)
 # treat testea cambios diferentes a un límite dado (lfc > x), eBayes() testea si hay diferencias respecto a 0
 fit_ebayes <- eBayes(fit_clusters)
 # se obtienen los top-ranked genes más expresados diferencialmente de cada contraste
@@ -441,12 +444,17 @@ genes_C1 <- topTable(fit_ebayes, coef = "C1all", number = Inf, p.value = 0.05, s
 genes_C2 <- topTable(fit_ebayes, coef = "C2all", number = Inf, p.value = 0.05, sort.by = "logFC")
 genes_C3 <- topTable(fit_ebayes, coef = "C3all", number = Inf, p.value = 0.05, sort.by = "logFC")
 genes_C4 <- topTable(fit_ebayes, coef = "C4all", number = Inf, p.value = 0.05, sort.by = "logFC")
+genes_C5 <- topTable(fit_ebayes, coef = "C5all", number = Inf, p.value = 0.05, sort.by = "logFC")
+genes_C6 <- topTable(fit_ebayes, coef = "C6all", number = Inf, p.value = 0.05, sort.by = "logFC")
 info_C1 <- head(genes_C1, 10) # si lfc = 0 salen muchos genes, pero si lfc = 1 solo se filtran 5!!
 info_C2 <- head(genes_C2, 10) # finalmente indico 0.95 para que salgan 11 genes en C1
 info_C3 <- head(genes_C3, 10) # con esto tenemos los genes en formato gene_id
 info_C4 <- head(genes_C4, 10)
+info_C5 <- head(genes_C5, 10)
+info_C6 <- head(genes_C6, 10)
 
 # 3- hacer el heatmap
+gene_info <- as.data.frame(SummarizedExperiment::rowData(rnaseq_se))
 # C1 ----------------------------------------------------------
 info_C1$gene_id <- rownames(info_C1)
 # la siguiente línea con match busca el valor de gene_name en gene_info antes definido
@@ -469,14 +477,37 @@ info_C4$gene_id <- rownames(info_C4)
 info_C4$gene_symbol <- gene_info$gene_name[match(info_C4$gene_id, rownames(gene_info))]
 info_C4$gene_cluster <- "C4"
 head(info_C4)
-# nos quedamos solo con las columnas que queremos y lo unimos todo
+# C5 ----------------------------------------------------------
+info_C4$gene_id <- rownames(info_C4)
+info_C4$gene_symbol <- gene_info$gene_name[match(info_C4$gene_id, rownames(gene_info))]
+info_C4$gene_cluster <- "C4"
+head(info_C4)
+# C4 ----------------------------------------------------------
+info_C4$gene_id <- rownames(info_C4)
+info_C4$gene_symbol <- gene_info$gene_name[match(info_C4$gene_id, rownames(gene_info))]
+info_C4$gene_cluster <- "C4"
+head(info_C4)
+# C5 ----------------------------------------------------------
+info_C5$gene_id <- rownames(info_C5)
+info_C5$gene_symbol <- gene_info$gene_name[match(info_C5$gene_id, rownames(gene_info))]
+info_C5$gene_cluster <- "C5"
+head(info_C5)
+# C6 ----------------------------------------------------------
+info_C6$gene_id <- rownames(info_C6)
+info_C6$gene_symbol <- gene_info$gene_name[match(info_C6$gene_id, rownames(gene_info))]
+info_C6$gene_cluster <- "C6"
+head(info_C6)
+
+#nos quedamos con las columnas que queremos y lo unimos todo
 columns_keep <- c("gene_id", "gene_symbol", "gene_cluster", "logFC", "adj.P.Val")
 info_C1 <- info_C1[, columns_keep]
 info_C2 <- info_C2[, columns_keep]
 info_C3 <- info_C3[, columns_keep]
 info_C4 <- info_C4[, columns_keep]
-genes_heatmap_info <- rbind(info_C1, info_C2, info_C3, info_C4)
-dim(genes_heatmap_info)
+info_C4 <- info_C4[, columns_keep]
+info_C5 <- info_C5[, columns_keep]
+info_C6 <- info_C6[, columns_keep]
+genes_heatmap_info <- rbind(info_C1, info_C2, info_C3, info_C4, info_C5, info_C6)
 
 # Segundo se construye la matriz de expresión
 # el objeto "v" creado por voom contiene la relación entre el gene_id
@@ -495,8 +526,8 @@ for (i in 1:nrow(mat_heatmap)) {
   mat_z[i, ] <- (mat_heatmap[i, ] - gene_mean)/gene_sd
 }
 # por último se crea la agrupación por clusters para las filas y las columnas
-gene_cluster <- factor(genes_heatmap_info$gene_cluster, levels = c("C1", "C2", "C3", "C4"))
-sample_cluster <- factor(cluster[colnames(mat_z)], levels = c("C1", "C2", "C3", "C4"))
+gene_cluster <- factor(genes_heatmap_info$gene_cluster, levels = c("C1", "C2", "C3", "C4", "C5", "C6"))
+sample_cluster <- factor(cluster[colnames(mat_z)], levels = c("C1", "C2", "C3", "C4", "C5", "C6"))
 
 # se buscan duplicados y missmatches
 anyDuplicated(genes_heatmap_info$gene_id)
@@ -506,13 +537,12 @@ stopifnot(identical(rownames(mat_z), genes_heatmap_info$gene_id))
 # heatmap
 # modificado para obtener los clusters ordenados y añadidos debajo como bloques
 # por colores
-cluster_colors <- c("C1" = "#E64B35","C2" = "#4DBBD5","C3" = "#00A087","C4" = "#F39B7F")
-
+cluster_colors <- c("C1" = "#E64B35","C2" = "#4DBBD5","C3" = "#00A087","C4" = "#F39B7F","C5" = "#3C5488","C6" = "#FFD700")
 # anotación inferior 1: clústers
 annotation_clusters <- HeatmapAnnotation(
   Cluster = anno_block(
     gp = gpar(fill = cluster_colors),
-    labels = c("C1", "C2", "C3", "C4"),
+    labels = c("C1", "C2", "C3", "C4", "C5", "C6"),
     labels_gp = gpar(col = "white", fontface = "bold")),
   which = "column")
 
@@ -530,8 +560,8 @@ subtype_colors <- c("CIN" = "#3C5488", "EBV" = "#00A087", "GS" = "#FFD700", "MSI
 # anotación inferior 2: Molecular subtype
 annotation_ms <- HeatmapAnnotation(
   Cluster = anno_block(
-    gp = gpar(fill = cluster_colors[c("C1", "C2", "C3", "C4")]),
-    labels = c("C1", "C2", "C3", "C4"),
+    gp = gpar(fill = cluster_colors[c("C1", "C2", "C3", "C4", "C5", "C6")]),
+    labels = c("C1", "C2", "C3", "C4", "C5", "C6"),
     labels_gp = gpar(col = "white",fontface = "bold")),
   Molecular_subtype = molecular_subtype,
   col = list(Molecular_subtype = subtype_colors),
@@ -556,6 +586,14 @@ Heatmap(
   column_gap = unit(2, "mm"),
   row_gap = unit(2, "mm")
 )
+
+# pendiente saber si obtener muestras más características por cluster para a partir
+# de ahí obtener la caracterización de clústers.
+
+# pendiente estudiar el paquete NMF para entenderlo y ver si supone una mejora
+# sobre consensusclusterplus. Estudiar si el método brunet es adecuado 
+# o se debería buscar otro.
+
 
 # Posteriormente se realizará un análisis de significación biológica para caracterizar cada clúster.
 # la intención es describir cada gen dentro de cada clúster, encontrar posibles incongruencias.
