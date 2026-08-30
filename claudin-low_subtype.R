5cb63bb4dd8469af804274b59a845e392572a0d2
Alejandro Sendín <asendinv@gmail.com>
2026-08-30 17:29:21 +0200

    Validation of gallo_score vs claudin-low samples wilcoxon, AUC, and correlation with marker genes

--- claudin-low_subtype.R
+++ claudin-low_subtype.R
@@ -12,7 +12,7 @@ for (pkg in bioc_packages) {
 }
 
 # install-packages-CRAN --------------------------------------------------------
-packages <- c("data.table", "dataframeexplorer", "devtools", "NMF")
+packages <- c()
 for (pkg in packages) {
   if (!requireNamespace(pkg, quietly = TRUE))
     install.packages(pkg)
@@ -22,7 +22,7 @@ for (pkg in packages) {
 libraries <- c("cBioPortalData", "TCGAbiolinks", "SummarizedExperiment", "dplyr",
                "ComplexHeatmap", "data.table", "dataframeexplorer", "devtools", 
                "cluster", "edgeR", 'limma', 'grid', 'ggplot2', 'PCAtools', 
-               'ConsensusClusterPlus', 'NMF', 'readxl', 'here')
+               'ConsensusClusterPlus', 'NMF', 'readxl', 'here', 'pROC')
 for (i in libraries) {
   library(i, character.only = TRUE)
 }
@@ -114,27 +114,119 @@ gallo_signature_old <- gallo_signature
 gene_check <- read.csv2(here("misc", "otros", "2025_Gallo_claudin-low_signature", "hgnc-symbol-check.csv"))
 # en este archivo hay simbolo nuevo vs simbolo antiguo (el que nos interesa)
 # ahora hay que seleccionar los 140 gene symbols de gallo + los 18 restantes
+# se obtienen los genes a actualizar de gene_signature old
 gene_check_match <- match(gallo_signature_old, gene_check$Input)
 genes_to_update <- !is.na(gene_check_match)
-gallo_signature_old[genes_to_update] <- gene_check$Approved.symbol[gene_check_match[genes_to_update]]
+# los genes a actualizar de gallo_signature_old se reemplazan por los antiguos
+# que existen en gene_info
+gallo_signature_old[genes_to_update] <- gene_check$Approved.symbol[
+  gene_check_match[genes_to_update]]
+# se obtienen los símboloes de los genes de las filas de tpm
 tpm_gene_symbol <- gene_info$gene_name[match(rownames(tpm), gene_info$gene_id)]
+# se construye el df
 tpm_df <- data.frame(
   gene_id = rownames(tpm),
   gene_symbol = tpm_gene_symbol,
   tpm,
   check.names = FALSE)
-tpm_gallo_df <- tpm_df[!is.na(tpm_df$gene_symbol) & 
+# se filtra por la firma de Gallo
+tpm_gallo <- tpm_df[!is.na(tpm_df$gene_symbol) & 
                          tpm_df$gene_symbol %in% gallo_signature_old, , drop = FALSE]
 
-dim(tpm_gallo_df)
-setdiff(gallo_signature_old, tpm_gallo_df$gene_symbol)
-anyDuplicated(tpm_gallo_df$gene_symbol)
+dim(tpm_gallo)
+setdiff(gallo_signature_old, tpm_gallo$gene_symbol)
+anyDuplicated(tpm_gallo$gene_symbol)
 
-tpm_gallo_df_log <- log2(tpm_gallo_df + 1)
-tpm_gallo_df_z <- t(scale(t(tpm_gallo_df_log)))
-
+rownames(tpm_gallo) <- tpm_gallo$gene_symbol
+tpm_gallo <- as.matrix(tpm_gallo_df[ , 
+                                     !colnames(tpm_gallo_df) %in% 
+                                       c("gene_id", "gene_symbol"), drop = FALSE])
+anyNA(tpm_gallo)
+anyDuplicated(rownames(tpm_gallo))
+# se transforma
+tpm_gallo_log <- log2(tpm_gallo + 1)
+# se obtiene el z-score
+tpm_gallo_z <- t(scale(t(tpm_gallo_log)))
+gallo_score <- apply(tpm_gallo_z, 2, median)
+# se incorpora la puntuación a annotation sabiendo que los nombres de muestra de
+# tpm y rna_aliquot_id son iguales
+annotation$gallo_score <- gallo_score[match(annotation$rna_aliquot_id, names(gallo_score))]
+# se usa la mediana porque es lo que hacen en Londero/Gallo, la media podría ser
+# una opción pero lo que buscamos es el valor típico más que el valor promedio
 
 # validation gallo_score mayor en 56 muestras claudin low
+# recogemos los valores de claudin_low_gallo dentro de annotation que NO son NA
+validation_df <- annotation[!is.na(annotation$claudin_low_gallo), ]
+# quedan fuera 16 muestras
+aggregate(gallo_score ~ claudin_low_gallo, # cuanto difieren los grupos? resume 
+          data = validation_df,            # gallo_score según grupos de claudin_low_gallo
+          FUN = function(x) {              
+            c(
+              n = length(x),
+              median = median(x),
+              IQR = IQR(x)
+            )
+            }
+          )
+
+# son las diferencias entre los grupos claudin-low y no low significativas?
+# wilcoxon para comparar distribuciones de gallo_score de los dos grupos sin 
+# depender de que siga una dist normal y teniendo grupos independientes
+wilcox.test(
+  gallo_score ~ claudin_low_gallo,
+  data = validation_df,
+  exact = FALSE
+)
+
+# se confirma que el score obtenido es claramente superior en las muestras 
+# claudin-low
+
+# se calcula el AUC
+roc_gallo <- pROC::roc(
+  response = validation_df$claudin_low_gallo,
+  predictor = validation_df$gallo_score,
+  levels = c(FALSE, TRUE),
+  direction = "<",
+  quiet = TRUE
+)
+
+pROC::auc(roc_gallo)
+# sensibilidad: claudin-low correctamente identificadas
+# especificidad: no claudin-low correctamente identificadas
+# se calcula cada uno en cada punto y se obtiene una métrica que resume la 
+# capacidad de discriminar entre los dos grupos
+# "<" indica que el gallo_score de FALSE (grupo control) debe ser menor que el
+# gallo_score de TRUE (grupo claudin-low) para que la predicción sea correcta.
+
+# genes validadores externos de la firma -> obtenidos en Gallo como genes muy 
+# relacionados con claudin-low
+marker_genes <- c("CLDN3", "CLDN4", "CLDN7", "CDH1", "VIM")
+# se evalua la correlación entre el gallo_score y la expresión de estos genes
+# se obtienen los valores de expresión de solamente los genes marcadores
+marker_tpm_df <- tpm_df[tpm_df$gene_symbol %in% marker_genes, , drop = FALSE]
+# se transforma a matriz quitando las columnas gene_id y gene_symbol
+marker_tpm <- as.matrix(marker_tpm_df[ , !colnames(marker_tpm_df) %in% 
+                                         c("gene_id", "gene_symbol"), drop = FALSE])
+# se cambia el nombre de las filas a las del gene_symbol y se ordenan las filas
+rownames(marker_tpm) <- marker_tpm_df$gene_symbol
+marker_tpm <- marker_tpm[marker_genes, , drop = FALSE]
+# se transforma
+marker_log <- log2(marker_tpm + 1)
+# se realiza la correlación
+marker_correlations <- data.frame(gene = marker_genes, rho = NA_real_, 
+                                  p_value = NA_real_)
+for (i in seq_along(marker_genes)) {
+  gene <- marker_genes[i]
+  correlation_test <- cor.test(annotation$gallo_score, marker_log[gene, ], 
+                               method = "spearman", exact = FALSE)
+  marker_correlations$rho[i] <- unname(correlation_test$estimate)
+  marker_correlations$p_value[i] <- correlation_test$p.value
+}
+marker_correlations$FDR <- p.adjust(marker_correlations$p_value, method = "BH")
+marker_correlations
+
+# se compara la expresión de estos genes marcadores entre caludin-low y no-low
+
 
 # calcular stromal, immune scores y pureza
 
